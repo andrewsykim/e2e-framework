86c3dc7358993e87157d64feab9fe95e81b0b339
Andrew Sy Kim <kim.andrewsy@gmail.com>
2021-06-30 17:31:16 -0400

    add testenv unit tests passing context values
    Signed-off-by: Andrew Sy Kim <kim.andrewsy@gmail.com>

--- pkg/env/env_test.go
+++ pkg/env/env_test.go
@@ -250,6 +250,31 @@ func TestEnv_Test(t *testing.T) {
 				return
 			},
 		},
+		{
+			name:     "with before-test, after-test using context",
+			ctx:      context.TODO(),
+			expected: 46,
+			setup: func(t *testing.T, ctx context.Context) int {
+				env := newTestEnv(conf.New())
+				// TODO: add test case using env.Setup once context propagation is supported from Setup
+				env.BeforeTest(func(ctx context.Context) (context.Context, error) {
+					return context.WithValue(ctx, &ctxKey{}, 44), nil
+				})
+				env.AfterTest(func(ctx context.Context) (context.Context, error) {
+					val, _ := ctx.Value(&ctxKey{}).(int)
+					val++
+					return context.WithValue(ctx, &ctxKey{}, val), nil
+				})
+				f := features.New("test-feat").Assess("assess", func(ctx context.Context, t *testing.T) context.Context {
+					val, _ := ctx.Value(&ctxKey{}).(int)
+					val++
+					return context.WithValue(ctx, &ctxKey{}, val)
+				})
+
+				ctx = env.Test(ctx, t, f.Feature())
+				return ctx.Value(&ctxKey{}).(int)
+			},
+		},
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
